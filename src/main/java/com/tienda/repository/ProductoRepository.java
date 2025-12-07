db175ab379ab87feefb6581de82f57663d69bfe3
DeibyVargasChaves <dvargas30884@ufide.ac.cr>
2025-12-07 17:29:03 -0600

    Semana 13 con correcciones aplicadas

--- src/main/java/com/tienda/repository/ProductoRepository.java
+++ src/main/java/com/tienda/repository/ProductoRepository.java
@@ -10,15 +10,23 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-public interface ProductoRepository extends JpaRepository<Producto, Integer> {
-
+public interface ProductoRepository extends JpaRepository<Producto, Integer>{
+    
+    //JPARepository -> CRUD + Consultas a la BD (SELECT, DELETE, LIST)
+     public List<Producto> findByActivoTrue();
+    
+    //JPA Ampliada
     public List<Producto> findByPrecioBetweenOrderByDescripcion(double precioInf, double precioSup);
-
-    @Query(value = "SELECT a FROM Producto a where a.precio BETWEEN :precioInf AND :precioSup ORDER BY a.descripcion ASC")
+    
+    // -> Query (1 -> Lista de productos que esten en el rango de precioInf y precioSup
+               //2 -> Ordenar por su Descripcion
+    
+    
+    //JPQL Ampliada
+    //Ejemplo de método utilizando Consultas con JPQL
+    @Query(value="SELECT a FROM Producto a where a.precio BETWEEN :precioInf AND :precioSup ORDER BY a.descripcion ASC")
     public List<Producto> metodoJPQL(@Param("precioInf") double precioInf, @Param("precioSup") double precioSup);
-
-    @Query(nativeQuery = true,
-            value = "SELECT * FROM Producto WHERE precio BETWEEN :precioInf AND :precioSup ORDER BY descripcion ASC")
-    public List<Producto> metodoNativo(@Param("precioInf") double precioInf, @Param("precioSup") double precioSup);
-
+    
+    
+    
 }
